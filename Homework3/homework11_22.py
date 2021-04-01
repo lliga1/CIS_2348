a102a67768da1acb10cbf97b09721555cc89bad6
Lyza Liga <lyzaliga1@gmail.com>
2021-03-31 19:13:31 -0500

    added code

--- Homework3/homework11_22.py
+++ Homework3/homework11_22.py
@@ -1,3 +1,11 @@
 # Marianel Liga
 # PSID 1394330
 # Homework 11.22
+
+words = input().split()
+for word in words:
+    count = 0
+    for n in words:
+        if n == word:
+            count += 1
+    print(word, count)
