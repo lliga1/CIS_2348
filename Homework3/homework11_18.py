c8f9b28c1a251a2063582f32988bcb1f1c5a3b0a
Lyza Liga <lyzaliga1@gmail.com>
2021-03-31 19:09:22 -0500

    added code

--- Homework3/homework11_18.py
+++ Homework3/homework11_18.py
@@ -1,3 +1,15 @@
 # Marianel Liga
 # PSID 1394330
 # Homework 11.18
+
+numbers = input().split()
+result = []
+
+for num in numbers:
+    num = int(num)
+    if num >= 0:
+        result.append(num)
+
+result.sort()
+for num in result:
+    print(num, end=' ')
